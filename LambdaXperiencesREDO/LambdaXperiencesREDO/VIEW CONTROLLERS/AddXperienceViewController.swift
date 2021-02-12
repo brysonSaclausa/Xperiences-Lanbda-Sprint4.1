d5922a309c8f92d3bc7b39b25ca7b2f628d5fc59
brysonS <brys1saclaus2@gmail.com>
2021-02-11 19:20:13 -0800

    Implement ImagePicker

--- LambdaXperiencesREDO/LambdaXperiencesREDO/VIEW CONTROLLERS/AddXperienceViewController.swift
+++ LambdaXperiencesREDO/LambdaXperiencesREDO/VIEW CONTROLLERS/AddXperienceViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreImage
+import Photos
 
 class AddXperienceViewController: UIViewController {
     
@@ -15,6 +17,12 @@ class AddXperienceViewController: UIViewController {
     @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     
+    var originalImage: UIImage? {
+        didSet {
+            updateImage()
+        }
+    }
+    
     //MARK: - LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +30,34 @@ class AddXperienceViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - METHODS
+    private func updateImage() {
+        if let originalImage = originalImage {
+            imageView.image = originalImage
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    private func presentImagePickerController() {
+            guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+                print("The photo library is not available.")
+                return
+            }
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            present(imagePicker, animated: true)
+        }
+    
+    
     //MARK: - IBACTIONS
     @IBAction func savePostButtonTapped(_ sender: Any) {
         
     }
     
     @IBAction func choosePhotoButtonTapped(_ sender: Any) {
+        presentImagePickerController()
     }
     
     
@@ -43,4 +73,22 @@ class AddXperienceViewController: UIViewController {
     }
     */
 
+}
+
+extension AddXperienceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.editedImage] as? UIImage {
+            originalImage = image
+        } else if let image = info[.originalImage] as? UIImage {
+            originalImage = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
